1f7d57f82bb4a6191abcb4ded384a3694654b5c5
Norman Munge <normanmunge@gmail.com>
2024-06-04 03:40:29 +0300

    Creates Player interface for type checking

---
+++ src/interfaces/player.ts
@@ -0,0 +1,19 @@
+export interface Player {
+  id: number;
+  first_name: string;
+  last_name: string;
+  date_of_birth: string;
+  country: string;
+  nationality: string;
+  position: string;
+  height: number;
+  weight: number;
+  image: string;
+  team_id: number;
+  created_at: string;
+  updated_at: string;
+  email: string;
+  phone_number: string;
+  gender: string;
+  password: string;
+}
